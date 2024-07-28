d9ae2ed497d6c38d2ee99f2ce6b79e8c10c5910d
aiarias <aiarias@uc.cl>
2024-07-27 20:27:32 -0400

    feat: add crud tasks

---
+++ src/routes/tasks.routes.js
@@ -0,0 +1,17 @@
+import Router from 'express';
+import { authRequired } from '../middlewares/validateToken.js';
+import { getTasks, getTask, updateTask, deleteTask, createTask } from '../controllers/tasks.controller.js';
+const router = Router()
+
+router.get('/tasks', authRequired, getTasks)
+router.post('/tasks', authRequired, createTask)
+router.delete('/tasks/:id', authRequired, deleteTask)
+router.get('/tasks/:id', authRequired, getTask)
+router.put('/tasks/:id', authRequired, updateTask) 
+
+
+
+
+
+
+export default router
